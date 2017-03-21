696fff261edac62fddae008f8331d48dfcbccf66
Elizabeth Merritt <lmerritt11@gmail.com>
2017-03-21 18:28:32 -0400

    Day 08 and 09 Work

---
+++ d09/materials/travelling-dwarf/solution.js
@@ -0,0 +1,120 @@
+console.log("WRITE YOUR SOLUTION IN HERE!");
+
+  // d.move();
+  // d.move(); 
+  // d.move(); 
+  // d.move();
+  // d.orient("down");
+  // d.move();
+  // d.move(); 
+  // d.move(); 
+  // d.move();
+  // d.move();
+  // d.move(); 
+  // d.move(); 
+  // d.move();
+  // d.move();
+  // d.move(); 
+  // d.move(); 
+  // d.move();
+  // d.move();
+  // d.move(); 
+  // d.move(); 
+  // d.move();
+  // d.orient("right");
+  // d.move();
+  // d.move();
+  // d.move();
+  // d.move(); 
+  // d.move(); 
+  // d.move();
+  // d.orient("up");
+  // d.move();
+  // d.move();
+  // d.move();
+  // d.move();
+  // d.move(); 
+  // d.move(); 
+  // d.move();
+  // d.move();
+  // d.move(); 
+  // d.move(); 
+  // d.move();
+  // d.orient("right");
+  // d.move(); 
+  // d.move();
+  // d.move();
+  // d.move(); 
+  // d.move(); 
+  // d.move();
+  // d.orient("down");
+  // d.move();
+  // d.move();
+  // d.move(); 
+  // d.move(); 
+  // d.move();
+  // d.move();
+  // d.move(); 
+  // d.move(); 
+  // d.move();
+  // d.orient("right");
+  // d.move();
+  // d.move();
+  // d.move();
+  // d.move();
+
+
+  // var num = 0;
+  // while (num < 4) {
+  //   d.move();
+  //   num = num + 1;
+  // }
+  // d.orient("down");
+    
+  // var num = 0;
+  // while (num < 16) {
+  //   d.move();
+  //   num = num + 1;
+  // }
+  // d.orient("right");
+
+  //   var num = 0;
+  // while (num < 6) {
+  //   d.move();
+  //   num = num + 1;
+  // }
+  // d.orient("up");
+
+  // var num = 0;
+  // while (num < 11) {
+  //   d.move();
+  //   num = num + 1;
+  // }
+
+  // d.orient("right");
+
+  // var num = 0;
+  // while (num < 6) {
+  //   d.move();
+  //   num = num + 1;
+  // }
+
+  // d.orient("down");
+
+  // var num = 0;
+  // while (num < 9) {
+  //   d.move();
+  //   num = num + 1;
+  // }
+
+  // d.orient("right");
+
+  // var num = 0;
+  // while (num < 2) {
+  //   d.move();
+  //   num = num + 1;
+  // }
+
+  //look up if statements, only want to make this move if it's an available spot//
+if (g.at === piece) {d.move = ()} else {d.orient = g.at === piece }
+
